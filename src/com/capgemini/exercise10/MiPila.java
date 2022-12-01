f00b1c842cc1504ff4ddef4dfdc3ba8a897e8cd3
Manrique <maria-paz.manrique@capgemini.com>
2022-12-01 17:00:23 +0100

    i want to sleep

--- src/com/capgemini/exercise10/MiPila.java
+++ src/com/capgemini/exercise10/MiPila.java
@@ -1,59 +1,47 @@
 package com.capgemini.exercise10;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MiPila<T> {
+	
 
-	public static void main(String[] args) {
-		Push(23);
-		peek();
-		pop();
-		reverse();
 
+
+	   private ArrayList<T> numbers=new ArrayList<T>();
+	    
+	   public void push(T e){
+	        numbers.add(e);
+	    }
+
+	   public T peek() {
+	        T lastElement = numbers.get(numbers.size()-1);
+	        return lastElement;
+	        
+	     
+	    }
+
+	   public T pop() {        
+	        T lastElement = numbers.remove(numbers.size() - 1);
+	        return lastElement;
+	    }
+	        
+	 
+	    
+	 
+	    
+	    public ArrayList<T> reverse() {
+	        ArrayList<T> revArrayList = new ArrayList<T>();
+	        for (int i = numbers.size() - 1; i >= 0; i--) {
+	       	 
+	            revArrayList.add(numbers.get(i));
+	        }
+	 
+	        numbers = revArrayList;
+	        return numbers;
 	}
-
-	private static ArrayList<Integer> numbers = new ArrayList<Integer>();
-
-	public static void Push(Integer number) {
-
-		numbers.add(number);
-
-		for (Integer position : numbers) {
-			System.out.println("Number = " + position);
-		}
-	}
-
-	public static int peek() {
-		int lastIdx = numbers.size() - 1;
-		Integer lastElement = numbers.get(lastIdx);
-
-		return lastElement;
-
-	}
-
-	public static ArrayList<Integer> reverse() {
-
-		ArrayList<Integer> revArrayList = new ArrayList<Integer>();
-		for (int i = numbers.size() - 1; i >= 0; i--) {
-
-			revArrayList.add(numbers.get(i));
-		}
-
-		numbers = revArrayList;
-		return numbers;
-	}
-
-	public static int pop() {
-		int lastIdx = numbers.size() - 1;
-		int lastElement = numbers.get(lastIdx);
-		numbers.remove(lastIdx);
-
-		return lastElement;
-
-	}
-
 	
 	
 	  
 		 
-}
+
